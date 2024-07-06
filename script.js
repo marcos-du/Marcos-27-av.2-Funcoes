77760b790987ebe581e570b7bec2898e6451fb1e
Marcos Eduardo <126473034+marcos-du@users.noreply.github.com>
2024-07-06 11:10:30 -0300

    script.js

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ const perguntas = [
         ]
     }
 ]; 
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
